bcdb78f761c5f2b425b74407e093bcb3ac512bb7
Florian Eiden <florian.eiden@fleid.fr>
2015-07-20 16:19:03 +0200

    Test Syntaxe

---
+++ u_tools/SyntaxTest.java
@@ -0,0 +1,16 @@
+package u_tools;
+
+public class SyntaxTest
+{
+	private int A;
+	
+	public void SyntaxTest( int A )
+	{
+		this.A = A;
+	}
+	
+	public void showMe()
+	{
+		System.out.println("This is it: "+ A);
+	}
+}
---
+++ u_tools/TestSyntaxTest.java
@@ -0,0 +1,14 @@
+package u_tools;
+
+public class TestSyntaxTest
+{	
+	public static void main( String[] args )
+	{
+		int A = 12;
+		
+		u_tools.SyntaxTest ST = new u_tools.SyntaxTest();
+		ST.SyntaxTest(A);
+		ST.showMe();
+
+	}
+}
