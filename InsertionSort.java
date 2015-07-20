ad1282a554e657dfe88211f3aaf1fdec23cb57e7
Florian Eiden <florian.eiden@fleid.fr>
2015-07-20 15:30:12 +0200

    InsertionSort

---
+++ InsertionSort.java
@@ -0,0 +1,36 @@
+import u_tools.*;
+
+public class InsertionSort
+{
+	public static void main( String[] args )
+	{
+		int[] aTrier = new int[] {10,2,8,6,7,3};
+		
+		u_tools.ArrayPrinter AP = new u_tools.ArrayPrinter();
+		
+		AP.pr( aTrier );
+		insertionSort( aTrier , AP );
+	}
+	
+	public static void insertionSort( int[] aTrier, u_tools.ArrayPrinter AP )
+	{
+		for ( int i = 1 ; i < aTrier.length ; i++ )
+		{
+			int current = aTrier[i];
+			int j = i - 1;
+			
+			while ( j >= 0 && aTrier[j] > current )
+			{
+				aTrier[j+1] = aTrier[j];
+				j = j - 1;
+			}
+			
+			aTrier[j+1] = current;
+			
+			AP.pr( aTrier );			
+		}
+		
+	}
+	
+	
+}
